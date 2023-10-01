21c993d5c26e97fd89a7924f438f9555655063bf
Aquib Mahmood <aquibmahmood106@gmail.com>
2023-10-01 20:12:50 +0530

    Check my database link for confidentiality of github

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { Client } from 'pg';
 
 export async function getClient() {
-    const client = new Client("postgres://rzgijigw:AGT1xDQqp980Larlm-ytUPcEwb3LIdzV@bubble.db.elephantsql.com/rzgijigw");
+    const client = new Client("postgres://rzgijigw:AGT1xDQqp980Larlm-ytUPcEwb3LIdzV@bubble.db.elephantsql.com/rzgixxxx");
     await client.connect();
     return client;
 }
